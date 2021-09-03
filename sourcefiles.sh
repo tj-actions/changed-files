89cdf514509acca7881bc1c4aa00e3b2407daaf9
Tonye Jack <jtonye@ymail.com>
2021-09-03 12:16:15 -0400

    Updated sourcefiles.sh

--- sourcefiles.sh
+++ sourcefiles.sh
@@ -9,7 +9,7 @@ FILES=()
 if [[ -n $INPUT_FILES_FROM_SOURCE_FILE ]]; then
   for file in $INPUT_FILES_FROM_SOURCE_FILE
   do
-    IFS=$'\n' read -r UNIQUE_FILES <<< "$(sort -u "$file")"
+    IFS=$'\n' read -r UNIQUE_FILES <<< "$(sort -u "$file" | tr "\n" " " | sed "s/.$//")"
     FILES+=( "${UNIQUE_FILES[@]}" )
   done
 fi
