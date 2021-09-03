c2f48e26e0ddefa0b24b71a54855f864ece7a5bb
Tonye Jack <jtonye@ymail.com>
2021-09-03 12:36:27 -0400

    Updated sourcefiles.sh

--- sourcefiles.sh
+++ sourcefiles.sh
@@ -9,11 +9,19 @@ FILES=()
 if [[ -n $INPUT_FILES_FROM_SOURCE_FILE ]]; then
   for file in $INPUT_FILES_FROM_SOURCE_FILE
   do
-    IFS=$'\n' read -r UNIQUE_FILES <<< "$(sort -u "$file" | tr "\n" " " | sed "s/.$//")"
-    FILES+=( "${UNIQUE_FILES[@]}" )
+    IFS=$'\n' read -r UNIQUE_FILES <<< "$file"
+    FILES+=("${UNIQUE_FILES[@]}")
   done
 fi
 
-echo "::set-output name=files::${FILES[*]}"
+FILES+=("${INPUT_FILES[@]}")
+
+echo "Input files: ${FILES[*]}"
+
+IFS=$'\n' read -r -a ALL_UNIQUE_FILES <<< "$(sort -u <<<"${FILES[*]}")"
+
+echo "All Unique Input files: ${ALL_UNIQUE_FILES[*]}"
+
+echo "::set-output name=files::${ALL_UNIQUE_FILES[*]}"
 
 echo "::endgroup::"
