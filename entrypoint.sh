156631c3ef4042a87ff5198fc310593cbdf8d4b7
Tonye Jack <jtonye@ymail.com>
2021-10-26 09:48:12 -0400

    Added debug lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,15 @@ fi
 
 SERVER_URL=$(echo "$GITHUB_SERVER_URL" | awk -F/ '{print $3}')
 
+echo "Setting up 'temp_changed_files' remote..."
+
 git ls-remote --exit-code temp_changed_files 1>/dev/null 2>&1 && exit_status=$? || exit_status=$?
 
 if [[ $exit_status -ne 0 ]]; then
+  echo "No 'temp_changed_files' remote found creating..."
   git remote add temp_changed_files "https://${INPUT_TOKEN}@${SERVER_URL}/${GITHUB_REPOSITORY}"
+else
+  echo "Found 'temp_changed_files' remote"
 fi
 
 echo "Getting HEAD info..."
