416132b56a394c0346035aaa132248f32ab1e2c1
Tonye Jack <jtonye@ymail.com>
2022-01-03 15:15:06 -0500

    Update sourcefiles.sh

--- sourcefiles.sh
+++ sourcefiles.sh
@@ -4,25 +4,23 @@ set -e
 
 echo "::group::changed-files-from-source-file"
 
-FILES=""
+IFS=" " read -r -a FILES <<< "$(echo "${INPUT_FILES[@]}" | sort -u | tr "\n" " ")"
 
 if [[ -n $INPUT_FILES_FROM_SOURCE_FILE ]]; then
   for file in $INPUT_FILES_FROM_SOURCE_FILE
   do
     while read -r fileName; do
-      FILES+=$(printf "%s\n" "$fileName")
+      FILES+=("$fileName")
     done <"$file"
   done
 fi
 
-FILES+=$INPUT_FILES
+echo "Input Files: ${FILES[*]}"
 
-printf "Input Files: %s" "$FILES"
+IFS=" " read -r -a ALL_UNIQUE_FILES <<< "$(echo "${FILES[@]}" | tr " " "\n" | sort -u | tr "\n" " ")"
 
-ALL_UNIQUE_FILES=$(printf "%s" "$FILES" | sort -u)
+echo "All Unique Input files: ${ALL_UNIQUE_FILES[*]}"
 
-printf "All Unique Input files: %s" "$ALL_UNIQUE_FILES"
-
-echo "::set-output name=files::$ALL_UNIQUE_FILES"
+echo "::set-output name=files::${ALL_UNIQUE_FILES[*]}"
 
 echo "::endgroup::"
