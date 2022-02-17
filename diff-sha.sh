0c4f022b4897e85663e541840fca345340ea0810
Tonye Jack <jtonye@ymail.com>
2022-02-17 01:34:40 -0500

    Updated diff-sha.sh

--- diff-sha.sh
+++ diff-sha.sh
@@ -74,7 +74,7 @@ else
     if [[ "$INPUT_USE_FORK_POINT" == "true" ]]; then
       echo "Getting fork point..."
       git fetch --no-tags -u --progress temp_changed_files "${TARGET_BRANCH}":"${TARGET_BRANCH}" && exit_status=$? || exit_status=$?
-      PREVIOUS_SHA=$(git merge-base --fork-point "${TARGET_BRANCH}" "${CURRENT_BRANCH}") && exit_status=$? || exit_status=$?
+      PREVIOUS_SHA=$(git merge-base --fork-point "${TARGET_BRANCH}") && exit_status=$? || exit_status=$?
       echo "$PREVIOUS_SHA"
     else
       git fetch --no-tags -u --progress --depth=1 temp_changed_files "${TARGET_BRANCH}":"${TARGET_BRANCH}" && exit_status=$? || exit_status=$?
