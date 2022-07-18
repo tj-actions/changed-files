2dd7420eeb1aecef398be6a18e03c62a8c907212
Tonye Jack <jtonye@ymail.com>
2022-07-18 15:00:32 -0400

    Update debug message.

--- diff-sha.sh
+++ diff-sha.sh
@@ -25,7 +25,7 @@ fi
 echo "::debug::Getting HEAD SHA..."
 
 if [[ -z $INPUT_SHA ]]; then
-  CURRENT_SHA=$(git rev-list --no-merges -n 1 HEAD 2>&1) && exit_status=$? || exit_status=$?
+  CURRENT_SHA=$(git rev-list --no-merges -n 1 "HEAD" 2>&1) && exit_status=$? || exit_status=$?
 else
   CURRENT_SHA=$INPUT_SHA && exit_status=$? || exit_status=$?
 fi
@@ -37,25 +37,33 @@ if [[ $exit_status -ne 0 ]]; then
   git --version
   echo "::error::You seem to be missing 'fetch-depth: 0' or 'fetch-depth: 2'. See https://github.com/tj-actions/changed-files#usage"
   exit 1
+else
+  echo "::debug::Current SHA: $CURRENT_SHA"
 fi
 
 if [[ -z $GITHUB_BASE_REF ]]; then
   TARGET_BRANCH=${GITHUB_REF/refs\/heads\//}
   CURRENT_BRANCH=$TARGET_BRANCH
 
+  echo "::debug::GITHUB_BASE_REF unset using $TARGET_BRANCH..."
+
   if [[ -z $INPUT_BASE_SHA ]]; then
     git fetch --no-tags -u --progress --depth=2 origin "${CURRENT_BRANCH}":"${CURRENT_BRANCH}" &&
     exit_status=$? || exit_status=$?
 
     if [[ $(git rev-list --count HEAD) -gt 1 ]]; then
-      PREVIOUS_SHA=$(git rev-list --no-merges -n 1 HEAD^1 2>&1) && exit_status=$? || exit_status=$?
+      PREVIOUS_SHA=$(git rev-list --no-merges -n 1 "HEAD^1" 2>&1) && exit_status=$? || exit_status=$?
+      echo "::debug::Previous SHA: $PREVIOUS_SHA"
     else
       PREVIOUS_SHA=$CURRENT_SHA && exit_status=$? || exit_status=$?
       echo "::debug::Initial commit detected"
+      echo "::debug::Previous SHA: $PREVIOUS_SHA"
     fi
   else
     PREVIOUS_SHA=$INPUT_BASE_SHA && exit_status=$? || exit_status=$?
     TARGET_BRANCH=$(git name-rev --name-only "$PREVIOUS_SHA" 2>&1) && exit_status=$? || exit_status=$?
+    echo "::debug::Previous SHA: $PREVIOUS_SHA"
+    echo "::debug::Target branch: $TARGET_BRANCH"
   fi
 
   git rev-parse --quiet --verify "$PREVIOUS_SHA^{commit}" 1>/dev/null 2>&1 && exit_status=$? || exit_status=$?
@@ -69,19 +77,25 @@ else
   TARGET_BRANCH=$GITHUB_BASE_REF
   CURRENT_BRANCH=$GITHUB_HEAD_REF
 
+  echo "::debug::GITHUB_BASE_REF: $TARGET_BRANCH..."
+
   if [[ -z $INPUT_BASE_SHA ]]; then
     if [[ "$INPUT_USE_FORK_POINT" == "true" ]]; then
       echo "::debug::Getting fork point..."
       git fetch --no-tags -u --progress origin "${TARGET_BRANCH}":"${TARGET_BRANCH}" && exit_status=$? || exit_status=$?
       PREVIOUS_SHA=$(git merge-base --fork-point "${TARGET_BRANCH}" "$(git name-rev --name-only "$CURRENT_SHA")") && exit_status=$? || exit_status=$?
+      echo "::debug::Previous SHA: $PREVIOUS_SHA"
     else
       git fetch --no-tags -u --progress origin --depth=1 "${TARGET_BRANCH}":"${TARGET_BRANCH}" && exit_status=$? || exit_status=$?
       PREVIOUS_SHA=$(git rev-list --no-merges -n 1 "${TARGET_BRANCH}" 2>&1) && exit_status=$? || exit_status=$?
+      echo "::debug::Previous SHA: $PREVIOUS_SHA"
     fi
   else
     git fetch --no-tags -u --progress origin --depth=1 "$INPUT_BASE_SHA" && exit_status=$? || exit_status=$?
     PREVIOUS_SHA=$INPUT_BASE_SHA
     TARGET_BRANCH=$(git name-rev --name-only "$PREVIOUS_SHA" 2>&1) && exit_status=$? || exit_status=$?
+    echo "::debug::Previous SHA: $PREVIOUS_SHA"
+    echo "::debug::Target branch: $TARGET_BRANCH"
   fi
 
   echo "::debug::Verifying commit SHA..."
