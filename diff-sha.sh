05f9744e418ca2abb86747d06d248d5416fff147
Tonye Jack <jtonye@ymail.com>
2022-09-24 19:33:47 -0600

    remove unused code

--- diff-sha.sh
+++ diff-sha.sh
@@ -66,9 +66,6 @@ if [[ -z $GITHUB_BASE_REF ]]; then
     if [[ -z "$PREVIOUS_SHA" || "$PREVIOUS_SHA" == "0000000000000000000000000000000000000000" ]]; then
       PREVIOUS_SHA=$(git rev-parse "$(git branch -r --sort=-committerdate | head -1)")
     fi
-    if [[ "$INPUT_SHA" == "$PREVIOUS_SHA" ]]; then
-      PREVIOUS_SHA=$(git rev-parse "$INPUT_SHA^1")
-    fi
 
     if [[ "$PREVIOUS_SHA" == "$CURRENT_SHA" ]]; then
       INITIAL_COMMIT="true"
