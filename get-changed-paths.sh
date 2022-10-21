dd5ec50e4b701a7c982dffccd3ba0bcf6e1bfe92
Tonye Jack <jtonye@ymail.com>
2022-10-21 06:58:29 -0600

    Update error handling.

--- get-changed-paths.sh
+++ get-changed-paths.sh
@@ -42,9 +42,19 @@ function get_diff() {
   done < <(git submodule | awk '{print $2}')
 
   if [[ "$INPUT_DIR_NAMES" == "true" ]]; then
-    git diff --diff-filter="$filter" --name-only --ignore-submodules=all "$base$DIFF$sha" | xargs -I {} dirname {} | uniq
+    git diff --diff-filter="$filter" --name-only --ignore-submodules=all "$base$DIFF$sha" | xargs -I {} dirname {} | uniq && exit_status=$? || exit_status=$?
+
+    if [[ $exit_status -ne 0 ]]; then
+      echo "::error::Failed to get changed directories between: $base$DIFF$sha"
+      exit 1
+    fi
   else
-    git diff --diff-filter="$filter" --name-only --ignore-submodules=all "$base$DIFF$sha"
+    git diff --diff-filter="$filter" --name-only --ignore-submodules=all "$base$DIFF$sha" && exit_status=$? || exit_status=$?
+
+    if [[ $exit_status -ne 0 ]]; then
+      echo "::error::Failed to get diff between: $base$DIFF$sha"
+      exit 1
+    fi
   fi
 }
 
@@ -65,9 +75,19 @@ function get_renames() {
   done < <(git submodule | awk '{print $2}')
 
   if [[ "$INPUT_DIR_NAMES" == "true" ]]; then
-    git log --name-status --ignore-submodules=all "$base$DIFF$sha" | grep -E "^R" | awk -F '\t' -v d="$INPUT_OLD_NEW_SEPARATOR" '{print $2d$3}' | xargs -I {} dirname {} | uniq
+    git log --name-status --ignore-submodules=all "$base$DIFF$sha" | grep -E "^R" | awk -F '\t' -v d="$INPUT_OLD_NEW_SEPARATOR" '{print $2d$3}' | xargs -I {} dirname {} | uniq && exit_status=$? || exit_status=$?
+
+    if [[ $exit_status -ne 0 ]]; then
+      echo "::error::Failed to get renamed directories between: $base$DIFF$sha"
+      exit 1
+    fi
   else
-    git log --name-status --ignore-submodules=all "$base$DIFF$sha" | grep -E "^R" | awk -F '\t' -v d="$INPUT_OLD_NEW_SEPARATOR" '{print $2d$3}'
+    git log --name-status --ignore-submodules=all "$base$DIFF$sha" | grep -E "^R" | awk -F '\t' -v d="$INPUT_OLD_NEW_SEPARATOR" '{print $2d$3}' && exit_status=$? || exit_status=$?
+
+    if [[ $exit_status -ne 0 ]]; then
+      echo "::error::Failed to get renames between: $base$DIFF$sha"
+      exit 1
+    fi
   fi
 }
 
