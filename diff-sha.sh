839d5701dbdeedacaaa60ae5c9a8106cccc6f78b
Tonye Jack <jtonye@ymail.com>
2022-11-15 10:29:27 -0700

    fix: bug with retrieving the last remote commit

--- diff-sha.sh
+++ diff-sha.sh
@@ -187,11 +187,7 @@ else
       PREVIOUS_SHA=$GITHUB_EVENT_BEFORE
 
       if ! git rev-parse --quiet --verify "$PREVIOUS_SHA^{commit}" 1>/dev/null 2>&1; then
-        PREVIOUS_SHA=$(git rev-list -n 1 "$TARGET_BRANCH" 2>&1) && exit_status=$? || exit_status=$?
-      fi
-
-      if [[ -z "$PREVIOUS_SHA" || "$PREVIOUS_SHA" == "0000000000000000000000000000000000000000" ]]; then
-        PREVIOUS_SHA=$(git rev-list -n 1 "$TARGET_BRANCH" 2>&1) && exit_status=$? || exit_status=$?
+        PREVIOUS_SHA=$(git rev-parse "$(git branch -r --sort=-committerdate | head -1)" 2>&1) && exit_status=$? || exit_status=$?
       fi
     else
       PREVIOUS_SHA=$(git rev-list -n 1 "$TARGET_BRANCH" 2>&1) && exit_status=$? || exit_status=$?
