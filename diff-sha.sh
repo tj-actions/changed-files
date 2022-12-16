ca30b8500a1363d8538291625cf74676f18993a9
Tonye Jack <jtonye@ymail.com>
2022-12-15 21:33:16 -0700

    Update diff-sha.sh

--- diff-sha.sh
+++ diff-sha.sh
@@ -227,12 +227,12 @@ else
       PREVIOUS_SHA=$GITHUB_EVENT_PULL_REQUEST_BASE_SHA
       
       if ! git diff --name-only --ignore-submodules=all "$PREVIOUS_SHA$DIFF$CURRENT_SHA" 1>/dev/null 2>&1; then
-        PREVIOUS_SHA=$(git merge-base "$TARGET_BRANCH" "$CURRENT_SHA") && exit_status=$? || exit_status=$?
+        PREVIOUS_SHA=$(git merge-base --fork-point "$TARGET_BRANCH" "$CURRENT_BRANCH") && exit_status=$? || exit_status=$?
       fi
     fi
 
     if [[ -z "$PREVIOUS_SHA" || "$PREVIOUS_SHA" == "$CURRENT_SHA" ]]; then
-      PREVIOUS_SHA=$(git merge-base --fork-point "$TARGET_BRANCH" "$CURRENT_SHA") && exit_status=$? || exit_status=$?
+      PREVIOUS_SHA=$(git merge-base --fork-point "$TARGET_BRANCH" "$CURRENT_BRANCH") && exit_status=$? || exit_status=$?
     fi
 
     echo "::debug::Previous SHA: $PREVIOUS_SHA"
