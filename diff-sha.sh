a7dbf42ba0d9340dd91c85959c3edb388d2055b2
Tonye Jack <jtonye@ymail.com>
2022-12-16 21:42:57 -0700

    updated to use target branch

--- diff-sha.sh
+++ diff-sha.sh
@@ -155,6 +155,9 @@ else
 
   if [[ "$INPUT_SINCE_LAST_REMOTE_COMMIT" == "false" ]]; then
     # shellcheck disable=SC2086
+    git fetch -u --progress $EXTRA_ARGS --depth="$INPUT_FETCH_DEPTH" origin +refs/heads/"$TARGET_BRANCH":refs/remotes/origin/"$TARGET_BRANCH" 1>/dev/null 2>&1
+    git branch --track "$TARGET_BRANCH" origin/"$TARGET_BRANCH" 1>/dev/null 2>&1 || true
+    # shellcheck disable=SC2086
     git fetch $EXTRA_ARGS -u --progress --depth=$(( GITHUB_EVENT_PULL_REQUEST_COMMITS + 1 + INPUT_FETCH_DEPTH )) origin "$GITHUB_REF":refs/remotes/origin/"$CURRENT_BRANCH" && exit_status=$? || exit_status=$?
   else
     # shellcheck disable=SC2086
@@ -206,7 +209,7 @@ else
         PREVIOUS_SHA=$(git rev-parse origin/"$CURRENT_BRANCH")
       fi
     else
-      PREVIOUS_SHA=$(git rev-list --max-parents=0 origin/"$CURRENT_BRANCH") && exit_status=$? || exit_status=$?
+      PREVIOUS_SHA=$(git rev-parse origin/"$TARGET_BRANCH") && exit_status=$? || exit_status=$?
     fi
   else
     PREVIOUS_SHA=$INPUT_BASE_SHA && exit_status=$? || exit_status=$?
