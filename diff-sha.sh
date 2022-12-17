7085b34b0c5202f962b369cd172cd5b86af491d8
Tonye Jack <jtonye@ymail.com>
2022-12-16 21:52:24 -0700

    updated to use target branch

--- diff-sha.sh
+++ diff-sha.sh
@@ -161,7 +161,7 @@ else
     git fetch $EXTRA_ARGS -u --progress --depth=$(( GITHUB_EVENT_PULL_REQUEST_COMMITS + 1 + INPUT_FETCH_DEPTH )) origin "$GITHUB_REF":refs/remotes/origin/"$CURRENT_BRANCH" && exit_status=$? || exit_status=$?
 
     if ! git rev-parse --symbolic-full-name --verify -q HEAD | grep -q "^refs/heads/"; then
-      git checkout -q "$GIT_BRANCH"
+      git checkout -q "$GITHUB_REF"
     fi
   else
     # shellcheck disable=SC2086
