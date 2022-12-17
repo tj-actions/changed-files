50b6410d7a57d8f5ef1769994ff6d28267490658
Tonye Jack <jtonye@ymail.com>
2022-12-17 15:26:22 -0700

    Update to catch error

--- diff-sha.sh
+++ diff-sha.sh
@@ -232,7 +232,7 @@ else
       PREVIOUS_SHA=${COMMON_ANCESTOR:-$GITHUB_EVENT_PULL_REQUEST_BASE_SHA}
 
       if ! git diff --name-only --ignore-submodules=all "$PREVIOUS_SHA$DIFF$CURRENT_SHA" 1>/dev/null 2>&1; then
-        PREVIOUS_SHA=$(git merge-base --all "$TARGET_BRANCH" HEAD | head -n 1)
+        PREVIOUS_SHA=$(git merge-base --all "$TARGET_BRANCH" HEAD | head -n 1) && exit_status=$? || exit_status=$?
       fi
     fi
 
