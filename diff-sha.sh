d42c82676c719c94b5c9642c88546d4b4cd05606
Tonye Jack <jtonye@ymail.com>
2022-12-18 17:58:19 -0700

    Updated to fetch all PR commits

--- diff-sha.sh
+++ diff-sha.sh
@@ -209,7 +209,10 @@ else
       echo "::debug::Fetching remote target branch..."
       # shellcheck disable=SC2086
       git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin "$TARGET_BRANCH" 1>/dev/null 2>&1
-      
+
+      # shellcheck disable=SC2086
+      git fetch $EXTRA_ARGS -u --progress --depth=$(( GITHUB_EVENT_PULL_REQUEST_COMMITS + 1 )) origin +"$GITHUB_REF":refs/remotes/origin/"$CURRENT_BRANCH" 1>/dev/null 2>&1
+
       PREVIOUS_SHA=$(git merge-base origin/"$TARGET_BRANCH" "$CURRENT_SHA") && exit_status=$? || exit_status=$?
 
       if [[ -z "$PREVIOUS_SHA" || "$PREVIOUS_SHA" == "$CURRENT_SHA" ]]; then
