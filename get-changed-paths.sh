f73938419c06d0edea8eb83eb12cea98c076f03f
Tonye Jack <jtonye@ymail.com>
2023-01-05 08:21:23 -0700

    Update get-changed-paths.sh

--- get-changed-paths.sh
+++ get-changed-paths.sh
@@ -58,7 +58,7 @@ function json_output() {
   fi
 
   # shellcheck disable=SC2086
-  jq $JQ_ARGS 'split("\n") | @json' | tr -s /
+  jq $JQ_ARGS 'split("\n") | @json' | sed -r 's/^"|"$//g' | tr -s /
 }
 
 function get_diff() {
