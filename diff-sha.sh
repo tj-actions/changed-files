fa6c1e94cc9c47a337da87e44082c79c3ff322dd
Tonye Jack <jtonye@ymail.com>
2023-01-18 15:26:26 -0700

    Fixed bug with push

--- diff-sha.sh
+++ diff-sha.sh
@@ -58,7 +58,7 @@ if [[ -z $GITHUB_EVENT_PULL_REQUEST_BASE_REF ]]; then
   if [[ -f .git/shallow ]]; then
     echo "Fetching remote refs..."
     # shellcheck disable=SC2086
-    git fetch $EXTRA_ARGS -u --progress origin "$CURRENT_BRANCH" 1>/dev/null
+    git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin "$CURRENT_BRANCH" 1>/dev/null
   fi
 
   echo "::debug::Getting HEAD SHA..."
@@ -157,7 +157,7 @@ else
   if [[ -f .git/shallow ]]; then
     echo "Fetching remote refs..."
     # shellcheck disable=SC2086
-    git fetch $EXTRA_ARGS -u --progress origin "$CURRENT_BRANCH" 1>/dev/null
+    git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin "$CURRENT_BRANCH" 1>/dev/null
   fi
 
   echo "::debug::Getting HEAD SHA..."
