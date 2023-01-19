57d9664f8e2aa45f26bcb59095f99aa47ae8e90d
Tonye Jack <jtonye@ymail.com>
2023-01-19 14:15:50 -0700

    Update diff-sha.sh

--- diff-sha.sh
+++ diff-sha.sh
@@ -63,7 +63,7 @@ if [[ -z $GITHUB_EVENT_PULL_REQUEST_BASE_REF ]]; then
   if [[ -f .git/shallow ]]; then
     echo "Fetching remote refs..."
     # shellcheck disable=SC2086
-    git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin "$CURRENT_BRANCH" 1>/dev/null
+    git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin "$CURRENT_BRANCH" 1>/dev/null || true
   fi
 
   echo "::debug::Getting HEAD SHA..."
