07dba6b85d18482fb68d20c4b936467b0b17baad
Tonye Jack <jtonye@ymail.com>
2023-02-15 19:43:05 -0700

    Fixed the test

--- diff-sha.sh
+++ diff-sha.sh
@@ -61,7 +61,6 @@ if [[ -z $GITHUB_EVENT_PULL_REQUEST_BASE_REF ]]; then
     echo "Fetching remote refs..."
     # shellcheck disable=SC2086
     git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin +refs/heads/"$CURRENT_BRANCH":refs/remotes/origin/"$CURRENT_BRANCH" 1>/dev/null
-    git branch --track "$CURRENT_BRANCH" origin/"$CURRENT_BRANCH" 1>/dev/null || true
   fi
 
   echo "::debug::Getting HEAD SHA..."
