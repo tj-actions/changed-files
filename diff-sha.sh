84ed30e2f4daf616144de7e0c1db59d5b33025e3
Tonye Jack <jtonye@ymail.com>
2023-03-09 21:01:01 -0700

    fix: error fetching history for PR branches (#1015)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- diff-sha.sh
+++ diff-sha.sh
@@ -188,13 +188,14 @@ else
 
   if [[ "$IS_SHALLOW" == "true" ]]; then
     echo "Fetching remote refs..."
-
-    if [[ "$GITHUB_EVENT_HEAD_REPO_FORK" != "true" ]]; then
-      # shellcheck disable=SC2086
-      git fetch $EXTRA_ARGS -u --progress origin pull/"$GITHUB_EVENT_PULL_REQUEST_NUMBER"/head:"$CURRENT_BRANCH" 1>/dev/null
+    
+    # shellcheck disable=SC2086
+    if git fetch $EXTRA_ARGS -u --progress origin pull/"$GITHUB_EVENT_PULL_REQUEST_NUMBER"/head:"$CURRENT_BRANCH" 1>/dev/null; then
+      echo "First fetch succeeded"
     else
+      echo "First fetch failed, falling back to second fetch"
       # shellcheck disable=SC2086
-      git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin +refs/heads/"$CURRENT_BRANCH"*:refs/remotes/origin/"$CURRENT_BRANCH"* 1>/dev/null
+      git fetch $EXTRA_ARGS -u --progress --deepen="$INPUT_FETCH_DEPTH" origin +refs/heads/"$CURRENT_BRANCH"*:refs/remotes/origin/"$CURRENT_BRANCH"* 1>/dev/null || true
     fi
     
     if [[ "$INPUT_SINCE_LAST_REMOTE_COMMIT" != "true" ]]; then
