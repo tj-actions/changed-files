bfa94533436ffe0d4df4627d1bc10f7ec9223aaf
GitHub Action <action@github.com>
2023-08-23 20:58:09 +0000

    Added missing changes and modified dist assets.

--- dist/index.js
+++ dist/index.js
@@ -82,7 +82,7 @@ const getRenamedFiles = ({ inputs, workingDirectory, hasSubmodule, diffResult, s
                     sha2: submoduleShaResult.currentSha,
                     diff
                 }))) {
-                    core.warning(`Set 'fetch_additional_submodule_history: true' to fetch additional submodule history for: ${submodulePath}`);
+                    core.warning(`Set 'fetch_additional_submodule_history: true' to fetch additional submodule history for: ${submodulePath}, Note you can control the fetch depth using 'fetch_depth' input`);
                     diff = '..';
                 }
                 const submoduleRenamedFiles = yield (0, utils_1.gitRenamedFiles)({
@@ -147,7 +147,7 @@ const getAllDiffFiles = ({ workingDirectory, hasSubmodule, diffResult, submodule
                     sha2: submoduleShaResult.currentSha,
                     diff
                 }))) {
-                    core.warning(`Set 'fetch_additional_submodule_history: true' to fetch additional submodule history for: ${submodulePath}`);
+                    core.warning(`Set 'fetch_additional_submodule_history: true' to fetch additional submodule history for: ${submodulePath}, Note you can control the fetch depth using 'fetch_depth' input`);
                     diff = '..';
                 }
                 const submoduleFiles = yield (0, utils_1.getAllChangedFiles)({
