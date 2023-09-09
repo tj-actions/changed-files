c8342b7384b4c699ce5c38a3544c27edee8e398c
GitHub Action <action@github.com>
2023-09-09 01:23:22 +0000

    Added missing changes and modified dist assets.

--- dist/index.js
+++ dist/index.js
@@ -1118,7 +1118,13 @@ const getSHAForPullRequestEvent = (inputs, env, workingDirectory, isShallow, has
                         0) {
                     throw new Error('Unable to locate the previous commit in the local history. Please ensure to checkout pull request HEAD commit instead of the merge commit. See: https://github.com/actions/checkout/blob/main/README.md#checkout-pull-request-head-commit-instead-of-merge-commit');
                 }
-                if (!previousSha) {
+                if (!previousSha ||
+                    (previousSha &&
+                        (yield (0, utils_1.verifyCommitSha)({
+                            sha: previousSha,
+                            cwd: workingDirectory,
+                            showAsErrorMessage: false
+                        })) !== 0)) {
                     throw new Error('Unable to locate the previous commit in the local history. Please ensure to checkout pull request HEAD commit instead of the merge commit. See: https://github.com/actions/checkout/blob/main/README.md#checkout-pull-request-head-commit-instead-of-merge-commit');
                 }
             }
