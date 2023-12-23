c9350d5a8a29b374927427eddfe6fa788983579b
Tonye Jack <jtonye@ymail.com>
2023-12-23 03:17:58 -0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1355,7 +1355,7 @@ export const setOutput = async ({
 
   // if safeOutput is true, escape special characters for bash shell
   if (safeOutput) {
-    cleanedValue = cleanedValue.replace(/[$()`|&;]/g, '\\$&')
+    cleanedValue = cleanedValue.replace(/[\x00-\x1F\x7F-\x9F\/\\:\*\?\"<>\|;`$()\&|\!]/g, '\\$&')
   }
 
   core.setOutput(key, cleanedValue)
