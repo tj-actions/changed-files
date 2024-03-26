e9e129c24b00a7c21535730063a88a64278347a9
Tonye Jack <jtonye@ymail.com>
2024-03-26 11:56:25 -0600

    fix: check for setting remote urls for forks

--- src/utils.ts
+++ src/utils.ts
@@ -745,21 +745,14 @@ export const getParentSha = async ({cwd}: {cwd: string}): Promise<string> => {
 }
 
 export const setForkRemote = async ({cwd}: {cwd: string}): Promise<void> => {
-  if (github.context.payload.repository?.fork) {
-    await exec.getExecOutput(
-      'git',
-      [
-        'remote',
-        'set-url',
-        'fork',
-        github.context.payload.repository?.clone_url
-      ],
-      {
-        cwd,
-        silent: !core.isDebug()
-      }
-    )
-  }
+  await exec.getExecOutput(
+    'git',
+    ['remote', 'set-url', 'fork', github.context.payload.repository?.clone_url],
+    {
+      cwd,
+      silent: !core.isDebug()
+    }
+  )
 }
 
 export const verifyCommitSha = async ({
