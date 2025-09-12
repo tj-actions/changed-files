3df2a2525d75c759fab45bc41419501b5d32a869
Tonye Jack <jtonye@ymail.com>
2025-09-12 04:48:56 -0600

    dependabot: npm_and_yarn/jest 30.1.3

--- dist/index.js
+++ dist/index.js
@@ -70617,7 +70617,7 @@ function blockString({ comment, type, value }, ctx, onComment, onChompKeep) {
     const { blockQuote, commentString, lineWidth } = ctx.options;
     // 1. Block can't end in whitespace unless the last line is non-empty.
     // 2. Strings consisting of only whitespace are best rendered explicitly.
-    if (!blockQuote || /\n[\t ]+$/.test(value) || /^\s*$/.test(value)) {
+    if (!blockQuote || /\n[\t ]+$/.test(value)) {
         return quotedString(value, ctx);
     }
     const indent = ctx.indent ||
